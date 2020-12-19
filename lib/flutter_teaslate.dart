9e71dcc53463474937e09abf75d6967129101698
Baptiste Vandenborght <bvandenborght@audassia.com>
2020-12-19 10:32:47 +0100

    First version 0.0.1

--- lib/flutter_teaslate.dart
+++ lib/flutter_teaslate.dart
@@ -1,7 +1,106 @@
 library flutter_teaslate;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+import 'package:flutter/cupertino.dart';
+
+class TeaSlate {
+  static const String apiUrl = "https://teaslate.twiiky.fr/api/translations/json";
+  final String key;
+  final bool debug;
+  final bool ignoreCertErrors;
+  bool isConnected = false;
+  List<dynamic> translations;
+
+  TeaSlate({@required this.key, this.debug = false, this.ignoreCertErrors = false});
+
+  Future<http.Response> _getAllTranslations() async {
+    try {
+      Map<String, String> headers = {
+        'Authorization': 'Token $key',
+      };
+
+      if (debug)
+        print("Will attempt to get $apiUrl with headers $headers");
+
+      HttpClient client = HttpClient();
+      if (ignoreCertErrors)
+        client = HttpClient()..badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+
+      var ioClient = IOClient(client);
+
+      final http.Response response = await ioClient.get(apiUrl, headers: headers).timeout((const Duration(seconds: 10)));
+
+      if (debug) {
+        print("GET Response status: ${response.statusCode}");
+        print("GET Response body: ${response.body}");
+      }
+
+      ioClient.close();
+
+      return response;
+    } on TimeoutException catch (_) {
+      Map<String, dynamic> error = {
+        'error': 'Timeout server',
+      };
+
+      if (debug)
+        print("GET Timeout $_");
+
+      return http.Response(json.encode(error), 504);
+    } on SocketException catch (_) {
+      if (debug)
+        print("GET Internal Server Error $_");
+      return _getAllTranslations();
+    } catch (e) {
+      if (debug)
+        print("POST Unhandled error $e");
+      return _getAllTranslations();
+    }
+  }
+
+  Future<bool> connect() async {
+    if (isConnected)
+      throw Exception("already connected");
+
+    http.Response response = await _getAllTranslations();
+
+    if (response.statusCode == 200) {
+      translations = json.decode(response.body);
+      isConnected = true;
+    } else if (response.statusCode == 400) {
+      if (debug)
+        print("error ${response.statusCode} - ${json.decode(response.body)['error']}");
+    } else {
+      if (debug)
+        print("error ${response.statusCode} - ${json.decode(response.body)}");
+    }
+
+    return isConnected;
+  }
+
+  String translate(String uid, {@required String lang}) {
+    if (isConnected) {
+      List<dynamic> translationsFound = translations.where((translation) => translation['uid'] == uid).toList();
+      if (translationsFound.length == 1) {
+        List<dynamic> translationsTexts = translationsFound.first['translation_set'].where((translation) => doesContain(translation['lang'], lang)).toList();
+        if (translationsTexts.length == 1) {
+          return translationsTexts.first['default'];
+        } else {
+          return translationsFound.first['translation_set'].first['default'];
+        }
+      } else {
+        throw Exception("UID $uid couldn't be found!");
+      }
+    } else {
+      throw Exception("not connected to API");
+    }
+  }
+}
+
+bool doesContain(String data, String containsText) {
+  return data.contains(containsText);
 }
--- test/flutter_teaslate_test.dart
+++ test/flutter_teaslate_test.dart
@@ -2,12 +2,37 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_teaslate/flutter_teaslate.dart';
 
+String API_KEY = "type_your_api_key_here";
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  print("Starting tests unit");
+
+  test('can connect to API', () {
+    String goodKey = API_KEY;
+    String badKey = "wrong";
+
+    TeaSlate teaslate = TeaSlate(key: badKey, debug: true, ignoreCertErrors: true);
+    expect(teaslate.connect().then((bool connected) => expect(connected, false)), completes);
+
+    TeaSlate teaslate_2 = TeaSlate(key: goodKey, debug: true, ignoreCertErrors: true);
+    expect(teaslate_2.connect().then((bool connected) => expect(connected, true)), completes);
   });
+
+  test('can retrieve a translation', () {
+    String translation = "whatIsYourName";
+
+    TeaSlate teaslate = TeaSlate(key: API_KEY, debug: true, ignoreCertErrors: true);
+
+    expect(teaslate.connect().then((bool connected) {
+      if (connected) {
+        expect(teaslate.translate(translation, lang:"en"), "What is your name?");
+        expect(teaslate.translate(translation, lang:"fr"), "Quel est votre nom ?");
+        expect(teaslate.translate(translation, lang:"hu"), "What is your name?", reason: "not found translation returns the first one");
+      } else {
+        fail("wasn't able to connect API");
+      }
+    }), completes);
+  });
+
+  print("Ending test units");
 }
